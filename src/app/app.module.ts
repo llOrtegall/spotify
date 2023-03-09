512c6bc8206629748cdce2f42b27bbd301ab3306
llOrtegall <IvanOrtega_97@hotmail.com>
2023-03-08 21:52:49 -0500

    sigue el ejemplo dado en menu component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
 @NgModule({
-  declarations: [
+  declarations: [ // declaraciones, componenntes, directivas pipes
     AppComponent
   ],
-  imports: [
+  imports: [ //Solo se importarn otrod modulos
     BrowserModule,
     AppRoutingModule
   ],
--- src/app/modules/home/home-routing.module.ts
+++ src/app/modules/home/home-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 
 const routes: Routes = [
   //Aqui definimos nuestras Rutas
+  {
+    path: '', //http: local/home/
+    component:HomePageComponent
+  }
 ];
 
 @NgModule({

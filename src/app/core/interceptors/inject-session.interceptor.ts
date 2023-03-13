365e46c2ca2591b0ced4da6f78e2887be3158e00
llOrtegall <IvanOrtega_97@hotmail.com>
2023-03-13 09:47:07 -0500

    comieza clase buscador input

--- src/app/core/interceptors/inject-session.interceptor.ts
+++ src/app/core/interceptors/inject-session.interceptor.ts
@@ -1,18 +1,37 @@
 import { Injectable } from '@angular/core';
 import {
-  HttpRequest,
-  HttpHandler,
-  HttpEvent,
-  HttpInterceptor
+    HttpRequest,
+    HttpHandler,
+    HttpEvent,
+    HttpInterceptor
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { CookieService } from 'ngx-cookie-service';
 
 @Injectable()
 export class InjectSessionInterceptor implements HttpInterceptor {
 
-  constructor() {}
+    constructor(private cookieService: CookieService) { }
 
-  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    return next.handle(request);
-  }
+    intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+
+        try {
+
+            const token = this.cookieService.get('token')
+
+            let newRequest = request
+            newRequest = request.clone({
+                setHeaders: {
+                    authorization: `Bearer ${token}`
+                }
+            })
+            return next.handle(newRequest);
+
+        } catch (error) {
+
+            console.log('Error Ojito', error)
+            return next.handle(request);
+
+        }
+    }
 }
